9a9ffc649f3e7ab66cf4badd55f1f34a7d386a09
xavier9712 <byro431@gmail.com>
2025-04-29 19:55:45 -0500

    Bechmarking

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) {
 
-        MetodosOrdenamiento ordenador = new MetodosOrdenamiento();
-        int[] arregloBurbuja = { 34, 2, 10, 6, 7, 5, 1, 15 };
-        int[] arregloSeleccion = { 34, 2, 10, 6, 7, 5, 1, 15 };
-        int[] arregloInsercion = { 34, 2, 10, 6, 7, 5, 1, 15 };
+        // MetodosOrdenamiento ordenador = new MetodosOrdenamiento();
+        // int[] arregloBurbuja = { 34, 2, 10, 6, 7, 5, 1, 15 };
+        // int[] arregloSeleccion = { 34, 2, 10, 6, 7, 5, 1, 15 };
+        // int[] arregloInsercion = { 34, 2, 10, 6, 7, 5, 1, 15 };
 
         // Insctucciones:
         // Todas las llamadas a los metodos deberan estar descomentadas y se debera
@@ -17,52 +17,52 @@ public class App {
         /// Anotar en cada metodo el error encontrado y como lo cambio en TEXTO
 
         // Metodo burbujaTradicional
-        int[] arregloBurbujaOrdenado1 = ordenador.burbujaTradicional(arregloBurbuja);
-        System.out.println(
-                "Resultado burbuja tradicional Metodo 1 : " +
-                        java.util.Arrays.toString(arregloBurbujaOrdenado1));
+        // int[] arregloBurbujaOrdenado1 = ordenador.burbujaTradicional(arregloBurbuja);
+        // System.out.println(
+        //         "Resultado burbuja tradicional Metodo 1 : " +
+        //                 java.util.Arrays.toString(arregloBurbujaOrdenado1));
 
-        // // Metodo burbujaTradicionalSegundo
-        int[] arregloBurbujaOrdenado2 =
-        ordenador.burbujaTradicionalSegundo(arregloBurbuja);
-        System.out.println(
-        "Resultado burbuja tradicional Metodo 2: " +
-        java.util.Arrays.toString(arregloBurbujaOrdenado2));
+        // // // Metodo burbujaTradicionalSegundo
+        // int[] arregloBurbujaOrdenado2 =
+        // ordenador.burbujaTradicionalSegundo(arregloBurbuja);
+        // System.out.println(
+        // "Resultado burbuja tradicional Metodo 2: " +
+        // java.util.Arrays.toString(arregloBurbujaOrdenado2));
 
-        // // // Metodo burbujaTradicionalTercero
-        int[] arregloBurbujaOrdenado3 =
-        ordenador.burbujaTradicionalTercero(arregloBurbuja);
-        System.out.println(
-        "Resultado burbuja tradicional Metodo 3: " +
-        java.util.Arrays.toString(arregloBurbujaOrdenado3));
+        // // // // Metodo burbujaTradicionalTercero
+        // int[] arregloBurbujaOrdenado3 =
+        // ordenador.burbujaTradicionalTercero(arregloBurbuja);
+        // System.out.println(
+        // "Resultado burbuja tradicional Metodo 3: " +
+        // java.util.Arrays.toString(arregloBurbujaOrdenado3));
 
-        // Metodo seleccionPrimero
-        int[] arregloSeleccionOrdenado1 =
-        ordenador.seleccionPrimero(arregloSeleccion);
-        System.out.println(
-        "Resultado selección Metodo 1: " +
-        java.util.Arrays.toString(arregloSeleccionOrdenado1));
+        // // Metodo seleccionPrimero
+        // int[] arregloSeleccionOrdenado1 =
+        // ordenador.seleccionPrimero(arregloSeleccion);
+        // System.out.println(
+        // "Resultado selección Metodo 1: " +
+        // java.util.Arrays.toString(arregloSeleccionOrdenado1));
 
-        // Metodo seleccionSegundo
-        int[] arregloSeleccionOrdenado2 =
-        ordenador.seleccionSegundo(arregloSeleccion);
-        System.out.println(
-        "Resultado selección Metodo 2: " +
-        java.util.Arrays.toString(arregloSeleccionOrdenado2));
+        // // Metodo seleccionSegundo
+        // int[] arregloSeleccionOrdenado2 =
+        // ordenador.seleccionSegundo(arregloSeleccion);
+        // System.out.println(
+        // "Resultado selección Metodo 2: " +
+        // java.util.Arrays.toString(arregloSeleccionOrdenado2));
 
-        // // Metodo seleccionTercero
-        int[] arregloSeleccionOrdenado3 =
-        ordenador.seleccionTercero(arregloSeleccion);
-        System.out.println(
-        "Resultado selección Metodo 3: " +
-        java.util.Arrays.toString(arregloSeleccionOrdenado3));
+        // // // Metodo seleccionTercero
+        // int[] arregloSeleccionOrdenado3 =
+        // ordenador.seleccionTercero(arregloSeleccion);
+        // System.out.println(
+        // "Resultado selección Metodo 3: " +
+        // java.util.Arrays.toString(arregloSeleccionOrdenado3));
 
-        // // Metodo insercionPrimero
-        int[] arregloInsercionOrdenado1 =
-        ordenador.insercionPrimero(arregloInsercion);
-        System.out.println(
-        "Resultado selección Metodo 1: " +
-        java.util.Arrays.toString(arregloInsercionOrdenado1));
+        // // // Metodo insercionPrimero
+        // int[] arregloInsercionOrdenado1 =
+        // ordenador.insercionPrimero(arregloInsercion);
+        // System.out.println(
+        // "Resultado selección Metodo 1: " +
+        // java.util.Arrays.toString(arregloInsercionOrdenado1));
 
         // // Metodo insercionSegundo
         // int[] arregloInsercionOrdenado2 =
@@ -78,5 +78,7 @@ public class App {
         // "Resultado selección Metodo 1: " +
         // java.util.Arrays.toString(arregloInsercionOrdenado3));
 
+
+        Benchmarking benchmarking = new Benchmarking();
     }
 }
---
+++ src/Benchmarking.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+public class Benchmarking {
+
+    private MetodosOrdenamiento mOrdenamiento;
+
+    public Benchmarking (){
+
+        long currentMillis = System.currentTimeMillis();
+        long currentNano = System.nanoTime();
+
+        System.out.println(currentMillis);
+        System.out.println(currentNano);
+
+        mOrdenamiento = new MetodosOrdenamiento();
+
+        int [ ] arreglo = genrrarArregloAleatorio(1000000);
+
+        Runnable tarea = ()-> mOrdenamiento.burbujaTradicional(arreglo);
+
+        double tiempoDuracionMillis= medirConCurrentTimeMiles(tarea);
+        double tiempoDuracionNano= medirConNanoTime(tarea);
+
+        System.out.println("Tiempo en millis: " + tiempoDuracionMillis);
+        System.out.println("Tiempo en nano: " + tiempoDuracionNano);
+
+    }
+
+    private int [ ] genrrarArregloAleatorio (int tamano ){
+        // 0 al 99.999
+        int [] arreglo = new int [tamano];
+        Random random = new Random();
+        
+        for ( int i = 0; i < tamano; i++) {
+            arreglo [i] = random.nextInt(100000);
+
+        }
+
+        return arreglo;
+
+    }
+    public double medirConCurrentTimeMiles(Runnable tarea){
+        long  inicio = System.currentTimeMillis();
+        tarea.run();
+        long fin = System.currentTimeMillis();
+        double tiempoSegundos = (fin - inicio) / 1000.0;
+        return tiempoSegundos;
+    }
+    public double medirConNanoTime (Runnable tarea){
+        long  inicio = System.currentTimeMillis();
+        tarea.run();
+        long fin = System.currentTimeMillis();
+        double tiempoSegundos = (fin - inicio) / 1000_000_000.0;
+        return tiempoSegundos;
+    }
+}
